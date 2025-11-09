69b02977f7ecbcb67b7b8830b04382db37b17ca8
gabrielnovaisgs-bmc <gabriel.novais@bymycell.com>
2025-11-09 09:59:42 -0300

    feat: update output configuration to map VM keys to their IPs

--- cloud/terraform-kvm-lab/output.tf
+++ cloud/terraform-kvm-lab/output.tf
@@ -1,6 +1,8 @@
 output "ips" {
-  value = [
-    for key, vm in local.vms : data.libvirt_domain_interface_addresses.ips[key].interfaces[0].addrs[0].addr
-  ]
-  description = "ips from vms"
+  value = { # for functions maps the key and the resource
+   # the outupt will be "domain": "ip"
+    for key, vm in local.vms : 
+      data.libvirt_domain_interface_addresses.ips[key].domain => data.libvirt_domain_interface_addresses.ips[key].interfaces[0].addrs[0].addr
+  }
+  description = "Map of VM keys to their IPs"
 }
