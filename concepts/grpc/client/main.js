25ffeba3dd45c06134f72d3c614b15ddcfd1b178
gabrielnovaisgs <gabrielnovaisgs@gmail.com>
2026-01-22 11:48:28 -0300

    feat: enhance gRPC server with error handling and response time metadata

--- concepts/grpc/client/main.js
+++ concepts/grpc/client/main.js
@@ -1,44 +1,89 @@
 import grpc from "@grpc/grpc-js";
 import protoLoader from "@grpc/proto-loader";
+import { randomUUID } from "node:crypto";
 
 const PROTO_PATH = "../calculator.proto";
-// Carregamos as definições do arquivo
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-});
 
-// Carregamos as deginições que realizamos do pacote em memória
-const calculatorProto = grpc.loadPackageDefinition(packageDefinition);
+async function main() {
+  const { command, data } = getCommandCli();
+  const { client, metadata } = configGrpcClient("secret");
 
-const client = new calculatorProto.Calculator(
-  "localhost:50051",
-  grpc.credentials.createInsecure(),
-);
+  const COMMANDS = {
+    div: () => execCommand(client, "Div", data, metadata),
+    sum: () => execCommand(client, "Sum", data, metadata),
+    mul: () => execCommand(client, "Multiply", data, metadata),
+    sub: () => execCommand(client, "Sub", data, metadata),
+  };
 
-const args = process.argv.slice(2);
-
-const data = {
-  first_number: args[1],
-  second_number: parseFloat(args[2]),
-};
-
-const commands = {
-  div: () => client.Div(data, printServerResult),
-  sum: () => client.Sum(data, printServerResult),
-  mul: () => client.Multiply(data, printServerResult),
-  sub: () => client.Sub(data, printServerResult),
-};
-
-commands[args[0]]();
-
-function printServerResult(err, response) {
-  if (err) {
-    console.log(`[ERROR]: ${err}`);
+  if (!COMMANDS[command]) {
+    console.error("Comando inválido!");
+    return;
   }
-
-  console.log(`[RESULT]:`, response);
+  const result = await COMMANDS[command]();
+  printServerResult(result.err, result.response, result.metadata);
 }
+
+function getCommandCli() {
+  const args = process.argv.slice(2);
+  return {
+    command: args[0],
+    data: {
+      first_number: args[1],
+      second_number: parseFloat(args[2]),
+    },
+  };
+}
+
+async function execCommand(client, commandClient, data, metadata) {
+  const result = await new Promise((resolve, reject) => {
+    let finalResponse;
+    const call = client[commandClient](data, metadata, (err, response) => {
+      if (err) return reject(err);
+
+      finalResponse = response;
+    });
+
+    call.on("status", (status) => {
+      resolve({
+        ...finalResponse,
+        metadata: status.metadata.getMap(),
+      });
+    });
+  });
+  return result;
+}
+
+function configGrpcClient(token) {
+  // Carregamos as definições do arquivo
+  const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+  });
+
+  // Carregamos as deginições que realizamos do pacote em memória
+  const calculatorProto = grpc.loadPackageDefinition(packageDefinition);
+  // adicionando metadados
+  const metadata = new grpc.Metadata();
+  metadata.add("auth-token", token);
+  metadata.add("request-id", randomUUID());
+  const client = new calculatorProto.Calculator(
+    "localhost:50051",
+    grpc.credentials.createInsecure(),
+  );
+  return { client, metadata };
+}
+
+function printServerResult(err, response, metadata) {
+  if (err) {
+    console.log(`[ERROR]: ${err.details}`);
+    console.log(`[STATUS]: ${err.code}`);
+    return;
+  }
+  console.log(`[RESULT]:`, response);
+  console.log(`[METADATA]: `, metadata);
+}
+
+main();
--- concepts/grpc/server/calc.py
+++ concepts/grpc/server/calc.py
@@ -2,26 +2,61 @@ import calculator_pb2
 import calculator_pb2_grpc
 import grpc
 from concurrent import futures
+import time
 class Calculator(calculator_pb2_grpc.CalculatorServicer):
-    def Multiply(self, request, context):
-        result = request.first_number * request.second_number
+    def Div(self, request, context):
+        start_time = time.time()
+        self.check_grpc_auth(context)
+        if request.second_number == 0:
+            context.abort(grpc.StatusCode.INVALID_ARGUMENT, 'Não é possível dividir por zero!') # Realizando o tratamento de erros
+            duration = time.time() - start_time
+            context.set_trailing_metadata([
+                    ('x-response-time', str(duration))
+                ])
+
+        result = request.first_number / request.second_number
+        duration = time.time() - start_time
+        context.set_trailing_metadata([
+            ('x-response-time', str(duration))
+        ])
 
         return calculator_pb2.CalcResponse(response=result) 
     
-    def Div(self, request, context):
-        if request.second_number == 0:
-            result = 0
-        else:
-            result = request.first_number / request.second_number
+    def Multiply(self, request, context):
+        start_time = time.time()
+        self.check_grpc_auth(context)
+        result = request.first_number * request.second_number
+        duration = time.time() - start_time
+        context.set_trailing_metadata([
+            ('x-response-time', str(duration))
+        ])
+
         return calculator_pb2.CalcResponse(response=result) 
     
     def Sum(self, request, context):
+        self.check_grpc_auth(context)
         result = request.first_number + request.second_number
         return calculator_pb2.CalcResponse(response=result) 
-    
+        
     def Sub(self, request, context):
+        self.check_grpc_auth(context)
         result = request.first_number - request.second_number
-        return calculator_pb2.CalcResponse(response=result) 
+        return calculator_pb2.CalcResponse(response=result)
+    
+    def check_grpc_auth(self, context):
+        metadata = dict(context.invocation_metadata())
+        token = metadata.get('auth-token')
+        if token != 'secret':
+            context.abort(grpc.StatusCode.UNAUTHENTICATED, 'Usuário não autenticado!')
+        return metadata
+    
+    def time_check(func):
+        def wrapper():
+            
+            func()
+            
+          
+    
     
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
