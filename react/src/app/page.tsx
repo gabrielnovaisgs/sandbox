8c9201d4ebd72bbe196573d39d99eb681a9870a0
gabrielnovaisgs <gabrielnovaisgs@gmail.com>
2026-02-18 11:19:07 -0300

    [feat]: code mirror

--- react/src/app/page.tsx
+++ react/src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       <Link href={"/pocs/reducer"}>Reducer</Link>
       <Link href={"/pocs/reducer-context"}>Reducer + API Context</Link>
       <Link href={"/pocs/drag-drop"}>Todo list drag and drop</Link>
+      <Link href={"/pocs/code-mirror"}>Code Mirror</Link>
     </div>
   );
 }
---
+++ react/src/app/pocs/code-mirror/page.tsx
@@ -0,0 +1,18 @@
+import {EditorState} from "@codemirror/state"
+import {EditorView, keymap} from "@codemirror/view"
+import {defaultKeymap} from "@codemirror/commands"
+
+let startState = EditorState.create({
+  doc: "Hello World",
+  extensions: [keymap.of(defaultKeymap)]
+})
+
+let view = new EditorView({
+  state: startState,
+  parent: document.body
+})
+
+export default function CodeMirrorPage() {
+    
+  return <div>Code Mirror Page</div>;
+}
